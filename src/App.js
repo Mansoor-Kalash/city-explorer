7b119ae7e2d529e59e4dd3dcb7732f516d03989f
Mansoor-Kalash <mansoor.jow@gmail.com>
2021-08-17 19:58:04 +0300

    lab-06 copleated

--- src/App.js
+++ src/App.js
@@ -1,77 +1,85 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Form';
- import axios from 'axios';
-import Card from 'react-bootstrap/Card';
-
-
-
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
 
 class App extends React.Component {
-  
   constructor(props) {
     super(props);
-    
-
     this.state = {
-      // arr:[],
       cityData: {},
-      show: false,
-      city:'',
-      
+      searchCity: "",
+      showData: false,
+      map: "",
     };
   }
- 
-   getData =async (e)=>{
-    
+
+  getLocation = async (e) => {
     e.preventDefault();
+
     await this.setState({
-
-city: e.target.citty.value,
+      searchCity: e.target.city.value,
     });
-    const response = await axios.get(`https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`);
-    console.log('aaaaaaaaaa',response);
-console.loge(this.state.citty);
+
+    let locURL = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchCity}&format=json`;
+
+    let resultData = await axios.get(locURL);
+
     this.setState({
-cityData: response.data[0],
-
+      cityData: resultData.data[0],
+      showData: true,
+      dataforcoun: "",
     });
-    console.log(this.state.cityData);
-  
-  }
+  };
 
-render() {
-    return 
-    <>
-  <>
-    
-
-    
-    <Form onSubmit={this.getLocation}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="text" placeholder="City Name" name="citty" />
-      </Form.Group>
-
- 
-          <Button variant="primary" type="submit">
+  render() {
+    return (
+      <div>
+        <>
+          <Form onSubmit={this.getLocation}>
+            <Form.Group>
+              <Form.Label>City</Form.Label>
+              <Form.Control type="text" placeholder="city name" name="city" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
               Submit
-           </Button>
-    </Form>
- </>
+            </Button>
+          </Form>
 
- <>
-  <Card style={{ width: '18rem' }}>
-  {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-  <Card.Body>
-    <Card.Title>{this.state.city}</Card.Title>
-    <Card.Text>lat: {this.state.cityData.lat} ion: {this.state.cityData.lon}</Card.Text>
+          {this.state.showData && (
+            <Card style={{ width: "18rem" }}>
+              <Card.Img
+                variant="top"
+                src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=18`}
+              />
+              <Card.Body>
+                <Card.Title>{this.state.searchCity}</Card.Title>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroupItem>
+                  {" "}
+                  {this.state.showData && <p>Lat:{this.state.cityData.lat} </p>}
+                </ListGroupItem>
+                <ListGroupItem>
+                  {" "}
+                  {this.state.showData && (
+                    <p>
+                      Lon:
+                      {this.state.cityData.lon}{" "}
+                    </p>
+                  )}
+                </ListGroupItem>
+              </ListGroup>
+            </Card>
+          )}
+        </>
+      </div>
+    );
+  }
+}
 
-  </Card.Body>
-</Card>
-</>
-</>
-
-
-}}
 export default App;
