e6f62839233c3d26311ddd98cd9591323d4d4d8a
Mansoor-Kalash <mansoor.jow@gmail.com>
2021-08-18 07:48:44 +0300

    Front End

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
       searchCity: "",
       showData: false,
       map: "",
+      weatherData:[],
     };
   }
 
@@ -26,13 +27,15 @@ class App extends React.Component {
     });
 
     let locURL = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchCity}&format=json`;
-
     let resultData = await axios.get(locURL);
+    let wData = await axios.get( `${process.env.REACT_APP_SERVER_LINK}/getweather?cityName=${this.state.searchCity}`);
+    console.log(wData.data);
+    
 
-    this.setState({
+    await this.setState({
       cityData: resultData.data[0],
       showData: true,
-      dataforcoun: "",
+      weatherData:wData.data,
     });
   };
 
@@ -52,32 +55,30 @@ class App extends React.Component {
 
           {this.state.showData && (
             <Card style={{ width: "18rem" }}>
-              <Card.Img
-                variant="top"
-                src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=18`}
-              />
+              <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=18`}/>
               <Card.Body>
                 <Card.Title>{this.state.searchCity}</Card.Title>
               </Card.Body>
               <ListGroup className="list-group-flush">
-                <ListGroupItem>
-                  {" "}
-                  {this.state.showData && <p>Lat:{this.state.cityData.lat} </p>}
-                </ListGroupItem>
-                <ListGroupItem>
-                  {" "}
-                  {this.state.showData && (
-                    <p>
-                      Lon:
-                      {this.state.cityData.lon}{" "}
-                    </p>
-                  )}
-                </ListGroupItem>
+                <ListGroupItem>{" "}{this.state.showData && <p>Lat:{this.state.cityData.lat} </p>}</ListGroupItem>
+                <ListGroupItem>{" "}{this.state.showData && (<p>Lon:{this.state.cityData.lon}{" "}</p>)}</ListGroupItem>
               </ListGroup>
             </Card>
           )}
         </>
-        
+        <>
+        {this.state.showList && this.state.weatherData.data.map((element, idx) => {
+            return (
+        <Card key={idx}>
+        <ListGroup>
+        <ListGroupItem>{" "}{this.state.showData && (<p>Lon: {element.datetime}{" "}</p>)}</ListGroupItem>
+        <ListGroupItem>{" "}{this.state.showData && (<p>Lon: {element.weather.description}{" "}</p>)}</ListGroupItem>
+        <ListGroupItem>{" "}{this.state.showData && (<p>Lon: {element.app_max_temp}{" "}</p>)}</ListGroupItem>
+        <ListGroupItem>{" "}{this.state.showData && (<p>Lon: {element.app_min_temp}{" "}</p>)}</ListGroupItem>
+        </ListGroup>
+        </Card>
+        );})}
+        </>
       </div>
     );
   }
