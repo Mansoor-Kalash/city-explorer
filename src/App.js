abd47cbd45d5c4a2c88603e0f50d378661cb064c
Mansoor-Kalashe <mansoor.jow@gmaile.com>
2021-08-19 08:57:47 +0300

    lab-08

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ class App extends React.Component {
       searchCity: "",
       showData: false,
       map: "",
-      weatherData:[],
+      weatherData: [],
+      weather5days:[],
     };
   }
 
@@ -25,20 +26,29 @@ class App extends React.Component {
     await this.setState({
       searchCity: e.target.city.value,
     });
-
+    //lab 06
     let locURL = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchCity}&format=json`;
     let resultData = await axios.get(locURL);
-    let wData = await axios.get( `${process.env.REACT_APP_SERVER_LINK}/getweather?cityName=${this.state.searchCity}`);
+    // lab 07
+    let wData = await axios.get(`${process.env.REACT_APP_SERVER_LINK}/getweather?cityName=${this.state.searchCity}`);
     console.log(wData.data);
     console.log(wData.data.data);
 
-    
+    // lab 08
+
+    let lab8weather = await axios.get(`${process.env.REACT_APP_SERVER_LINK}/weather?cityName=${this.state.searchCity}`);
+
+
+
+
 
     await this.setState({
       cityData: resultData.data[0],
       showData: true,
-      weatherData:wData.data.data,
+      weatherData: wData.data.data,
+      weather5days: lab8weather.data,
     });
+    console.log('55555555',this.state.weather5days);
   };
 
   render() {
@@ -57,7 +67,7 @@ class App extends React.Component {
 
           {this.state.showData && (
             <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={`${process.env.REACT_APP_SERVER_LINK}?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=18`}/>
+              <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=18`} />
               <Card.Body>
                 <Card.Title>{this.state.searchCity}</Card.Title>
               </Card.Body>
@@ -69,18 +79,34 @@ class App extends React.Component {
           )}
         </>
         <>
-        {this.state.weatherData.map((element, idx) => {
+          {this.state.weatherData.map((element, idx) => {
             return (
-        <Card key={idx}>
-        <ListGroup>
-        <ListGroupItem> {element.datetime}</ListGroupItem>
-        <ListGroupItem> {element.weather.description}</ListGroupItem>
-        <ListGroupItem> {element.app_max_temp}</ListGroupItem>
-        <ListGroupItem> {element.app_min_temp}</ListGroupItem>
-        </ListGroup>
-        </Card>
-        );})}
+              <Card key={idx}>
+                <ListGroup>
+                  <ListGroupItem> {element.datetime}</ListGroupItem>
+                  <ListGroupItem> {element.weather.description}</ListGroupItem>
+                  <ListGroupItem> {element.app_max_temp}</ListGroupItem>
+                  <ListGroupItem> {element.app_min_temp}</ListGroupItem>
+                </ListGroup>
+              </Card>
+            );
+          })}
         </>
+
+        {/* <>
+          {this.state.weatherData.map((element, idx) => {
+            return (
+              <Card key={idx}>
+                <ListGroup>
+                  <ListGroupItem> {element.datetime}</ListGroupItem>
+                  <ListGroupItem> {element.weather.description}</ListGroupItem>
+                  <ListGroupItem> {element.app_max_temp}</ListGroupItem>
+                  <ListGroupItem> {element.app_min_temp}</ListGroupItem>
+                </ListGroup>
+              </Card>
+            );
+          })}
+        </> */}
       </div>
     );
   }
