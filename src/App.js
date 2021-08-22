d669229e6943fded5a4fad25bc4016b287dc6d45
Mansoor-Kalash <mansoor.jow@gmail.com>
2021-08-22 07:37:41 +0300

    lab-10 copleted

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 import ListGroupItem from "react-bootstrap/ListGroupItem";
-import  Movies from "./component/Movies";
-import  Weathers from "./component/Weathers";
-
+import Movies from "./component/Movies";
+import Weathers from "./component/Weathers";
 
 class App extends React.Component {
   constructor(props) {
@@ -19,8 +18,8 @@ class App extends React.Component {
       showData: false,
       map: "",
       weatherData: [],
-      weather5days:[],
-      moviesData :[],
+      weather5days: [],
+      moviesData: [],
       errorMsg: false,
     };
   }
@@ -33,85 +32,98 @@ class App extends React.Component {
     });
     try {
       //lab 06
-    let resultData =await axios.get( `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchCity}&format=json`);
-    // lab 07
-    let wData = await axios.get(`${process.env.REACT_APP_SERVER_LINK}/getweather?cityName=${this.state.searchCity}`);
+      let resultData = await axios.get(
+        `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchCity}&format=json`
+      );
+      // lab 07
+      let wData = await axios.get(
+        `${process.env.REACT_APP_SERVER_LINK}/getweather?cityName=${this.state.searchCity}`
+      );
 
-    // lab 08
+      // lab 08
 
-    let lab8weather = await axios.get(`${process.env.REACT_APP_SERVER_LINK}/weather?cityName=${this.state.searchCity}`);
+      let lab8weather = await axios.get(
+        `${process.env.REACT_APP_SERVER_LINK}/weather?cityName=${this.state.searchCity}`
+      );
 
-    let lab8movies = await axios.get(`${process.env.REACT_APP_SERVER_LINK}/movies?moviName=${this.state.searchCity}`);
+      let lab8movies = await axios.get(
+        `${process.env.REACT_APP_SERVER_LINK}/movies?moviName=${this.state.searchCity}`
+      );
 
-    await this.setState({
-      cityData: resultData.data[0],
-      showData: true,
-      weatherData: wData.data.data,
-      weather5days: lab8weather.data,
-      moviesData : lab8movies.data,
-      errorMsg : false,
-    });
-
+      await this.setState({
+        cityData: resultData.data[0],
+        showData: true,
+        weatherData: wData.data.data,
+        weather5days: lab8weather.data,
+        moviesData: lab8movies.data,
+        errorMsg: false,
+      });
     } catch (error) {
       await this.setState({
         cityData: {},
         showData: false,
         weatherData: [],
         weather5days: [],
-        moviesData : [],
-        errorMsg : true,
+        moviesData: [],
+        errorMsg: true,
       });
     }
-    
   };
 
   render() {
     return (
       <div>
-        
-          <Form onSubmit={this.getLocation}>
-            <Form.Group>
-              <Form.Label>City</Form.Label>
-              <Form.Control type="text" placeholder="city name" name="city" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
+        <Form onSubmit={this.getLocation}>
+          <Form.Group>
+            <Form.Label>City</Form.Label>
+            <Form.Control type="text" placeholder="city name" name="city" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
 
-          {this.state.showData && (
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=18`} />
-              <Card.Body>
-                <Card.Title>{this.state.searchCity}</Card.Title>
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroupItem>{" "}{this.state.showData && <p>Lat:{this.state.cityData.lat} </p>}</ListGroupItem>
-                <ListGroupItem>{" "}{this.state.showData && (<p>Lon:{this.state.cityData.lon}{" "}</p>)}</ListGroupItem>
-              </ListGroup>
-            </Card>
-          )}
-        
-        
-        {this.state.showData && <Weathers show= {this.state.showData} weatherData={this.state.weatherData}/>}
-        
+        {this.state.showData && (
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=18`}
+            />
+            <Card.Body>
+              <Card.Title>{this.state.searchCity}</Card.Title>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>
+                {" "}
+                {this.state.showData && <p>Lat:{this.state.cityData.lat} </p>}
+              </ListGroupItem>
+              <ListGroupItem>
+                {" "}
+                {this.state.showData && <p>Lon:{this.state.cityData.lon} </p>}
+              </ListGroupItem>
+            </ListGroup>
+          </Card>
+        )}
 
-       
-         
-              {this.state.showData && <Movies show= {this.state.showData} moviesData={this.state.moviesData}/>}
-           
-         
-        
-        {
-            this.state.errorMsg &&
+        {this.state.showData && (
+          <Weathers
+            show={this.state.showData}
+            weatherData={this.state.weatherData}
+          />
+        )}
 
+        {this.state.showData && (
+          <Movies
+            show={this.state.showData}
+            moviesData={this.state.moviesData}
+          />
+        )}
 
-            <div role="alert">
-              <strong>Error!something doesn't work out </strong>
-              
-            </div>
-
-          }
+        {this.state.errorMsg && (
+          <div role="alert">
+            <strong>Error!something doesn't work out </strong>
+          </div>
+        )}
       </div>
     );
   }
